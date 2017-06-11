75bc7363236d9c6a301933ab0bbda6c01b48cb14
Marco Silverio <marcosilv7486@gmail.com>
2017-06-10 20:35:10 -0500

    Commit de prueba

--- Estandar/General.cs
+++ Estandar/General.cs
@@ -40,7 +40,7 @@ namespace Estandar
         {
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        private void label7_Click(object sender,  EventArgs e)
         {
 
         }
