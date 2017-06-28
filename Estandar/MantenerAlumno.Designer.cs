525512ebf3c62f81c9cc94ba52a97956937956dd
Marco Silverio <marcosilv7486@gmail.com>
2017-06-28 01:06:41 -0500

    mantenimiento de alumnos

--- Estandar/MantenerAlumno.Designer.cs
+++ Estandar/MantenerAlumno.Designer.cs
@@ -29,40 +29,166 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dtDatos = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dtDatos)).BeginInit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFiltroDocumento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(319, 9);
+            this.label1.Location = new System.Drawing.Point(278, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(296, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Mantenimiento de Alumnos";
             // 
-            // dtDatos
+            // label4
             // 
-            this.dtDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtDatos.Location = new System.Drawing.Point(12, 68);
-            this.dtDatos.Name = "dtDatos";
-            this.dtDatos.Size = new System.Drawing.Size(858, 267);
-            this.dtDatos.TabIndex = 4;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Número de documento";
+            // 
+            // txtFiltroDocumento
+            // 
+            this.txtFiltroDocumento.Location = new System.Drawing.Point(629, 77);
+            this.txtFiltroDocumento.Name = "txtFiltroDocumento";
+            this.txtFiltroDocumento.Size = new System.Drawing.Size(134, 20);
+            this.txtFiltroDocumento.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nombres Y Apellidos";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(341, 77);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(161, 20);
+            this.txtNombre.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Código Alumno";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(101, 77);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(111, 20);
+            this.txtCodigo.TabIndex = 9;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader8,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(11, 103);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(840, 200);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.DisplayIndex = 7;
+            this.columnHeader8.Text = "id";
+            this.columnHeader8.Width = 0;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 0;
+            this.columnHeader1.Text = "Código";
+            this.columnHeader1.Width = 75;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 1;
+            this.columnHeader2.Text = "Nombre";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 2;
+            this.columnHeader3.Text = "Apellidos";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.DisplayIndex = 3;
+            this.columnHeader4.Text = "Documento";
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.DisplayIndex = 4;
+            this.columnHeader5.Text = "Número";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.DisplayIndex = 5;
+            this.columnHeader6.Text = "Programa";
+            this.columnHeader6.Width = 150;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.DisplayIndex = 6;
+            this.columnHeader7.Text = "Plan Curricular";
+            this.columnHeader7.Width = 100;
             // 
             // MantenerAlumno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 347);
-            this.Controls.Add(this.dtDatos);
+            this.ClientSize = new System.Drawing.Size(858, 312);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFiltroDocumento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "MantenerAlumno";
             this.Text = "MantenerAlumno";
             this.Load += new System.EventHandler(this.MantenerAlumno_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dtDatos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +197,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dtDatos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFiltroDocumento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
     }
 }
--- Estandar/MantenerAlumno.cs
+++ Estandar/MantenerAlumno.cs
@@ -8,22 +8,86 @@ using System.Text;
 using System.Windows.Forms;
 using  Dominio;
 using Negocio;
+using Negocio;
+using Dominio;
 namespace Estandar
 {
     public partial class MantenerAlumno : Form
     {
 
         private IGestionTesis gestionTesis;
+        private List<Alumno> alumnos;
+        private Alumno alumno;
 
         public MantenerAlumno()
         {
             InitializeComponent();
             gestionTesis = new GestionTesis();
+            listView1.View = View.Details;
+            cargarDatos();
+            listView1.DoubleClick += new EventHandler(listView1_DoubleClick);
+        }
+
+        void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            int idSeleccionado = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            alumno = alumnos.Find(a => a.id.Equals(idSeleccionado));
+        }
+
+        private void cargarDatos()
+        {
+            alumnos = gestionTesis.obtenerAlumnosHabilitados();
+            foreach (Alumno alumno in alumnos)
+            {
+
+                listView1.Items.Add(generarAlumno(alumno));
+
+            }
+        }
+
+        private ListViewItem generarAlumno(Alumno alumno)
+        {
+            ListViewItem listitem = new ListViewItem(alumno.id.ToString());
+            listitem.SubItems.Add(alumno.codigo);
+            listitem.SubItems.Add(alumno.nombre);
+            listitem.SubItems.Add(alumno.apellidos);
+            listitem.SubItems.Add(alumno.tipoDocumento);
+            listitem.SubItems.Add(alumno.numeroDocumento);
+            listitem.SubItems.Add(alumno.programaPostGrado.nombrePrograma);
+            listitem.SubItems.Add(alumno.planCurricular);
+            return listitem;
+        }
+
+    
+
+        void txtNombre_KeyUp(object sender, KeyEventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Items.AddRange(alumnos.Where(i => string.IsNullOrEmpty(txtNombre.Text) || i.nombreCompleto().ToLower().Contains(txtNombre.Text.ToLower()))
+            .Select(c => generarAlumno(c)).ToArray());
+        }
+
+        void txtFiltroDocumento_KeyUp(object sender, KeyEventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Items.AddRange(alumnos.Where(i => string.IsNullOrEmpty(txtFiltroDocumento.Text) || i.numeroDocumento.StartsWith(txtFiltroDocumento.Text))
+            .Select(c => generarAlumno(c)).ToArray());
+        }
+
+        void txtCodigo_KeyUp(object sender, KeyEventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Items.AddRange(alumnos.Where(i => string.IsNullOrEmpty(txtCodigo.Text) || i.codigo.StartsWith(txtCodigo.Text))
+            .Select(c => generarAlumno(c)).ToArray());
         }
 
         private void MantenerAlumno_Load(object sender, EventArgs e)
         {
-            dtDatos.DataSource = gestionTesis.obtenerTodos().Tables[0];
+            txtCodigo.KeyUp += new KeyEventHandler(txtCodigo_KeyUp);
+            txtFiltroDocumento.KeyUp += new KeyEventHandler(txtFiltroDocumento_KeyUp);
+            txtNombre.KeyUp += new KeyEventHandler(txtNombre_KeyUp);
         }
+
+
     }
 }
